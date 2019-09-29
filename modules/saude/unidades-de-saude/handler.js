2d257ab18f2fe11760d1854392ccbd6a9d2f69c5
ricardoabrito <ricardoabdantas@gmail.com>
2019-09-29 18:52:05 -0300

    atualizando localização de funções para não ocorrer conflitos

--- modules/saude/unidades-de-saude/handler.js
+++ modules/saude/unidades-de-saude/handler.js
@@ -7,36 +7,36 @@ const internal_secret = fs.readFileSync('/var/openfaas/secrets/internal-secret',
 
 const url = "http://gateway:8080/function/mongo-service/unidades-de-saude/find";
 
+const buildQuery = (data) => {
+
+  var query = { "location":
+                { "$near":
+                  { "$geometry":
+                    { 
+                      "type": "Point" ,
+                      "coordinates": [ data.longitude, data.latitude] 
+                    },
+                    "$maxDistance": data.distancia >= 10000 ? 10000 : data.distancia
+                  } 
+                }
+              };
+
+  return query;
+
+}
+
+const buildHeader = () => {
+  var header = {
+      'x-http-internal-secret':  internal_secret,
+      'Content-Type': 'application/json'
+  };
+  return {'headers': header};
+}
+
 module.exports = (event, context) => {
     axios.post(url, buildQuery(event.body), buildHeader()).then(function (response) {
         context.status(200).succeed(response.data.result);
     }).catch(function (error) {
         context.status(500).succeed(error);
     });
-}
-
-function buildQuery(data){
-
-    var query = { "location":
-                  { $near:
-                    { $geometry:
-                      { 
-                        "type": "Point" ,
-                        "coordinates": [ data.longitude, data.latitude] 
-                      },
-                      $maxDistance: data.distancia >= 10000 ? 10000 : data.distancia
-                    } 
-                  }
-                };
-
-    return query;
-
-}
-
-function buildHeader(){
-    var header = {
-        'x-http-internal-secret':  internal_secret,
-        'Content-Type': 'application/json'
-    };
-    return {'headers': header};
 }
